041755f93b38e75ac9fd2bb8f9eb23e57dcb6b58
Tanishq2505 <tanishqa@89@gmail.com>
2022-09-03 12:05:38 +0530

    :construction: Base navigation bar added

--- lib/presentation/screens/main_dashboard.dart
+++ lib/presentation/screens/main_dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MainDashboard extends StatefulWidget {
   const MainDashboard({Key? key}) : super(key: key);
@@ -8,10 +10,53 @@ class MainDashboard extends StatefulWidget {
 }
 
 class _MainDashboardState extends State<MainDashboard> {
+  int _selectedIndex = 0;
+  onTabChange(int? index) {
+    if (index != null) {
+      _selectedIndex = index;
+      print(_selectedIndex);
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: GNav(
+          onTabChange: onTabChange,
+          haptic: true,
+          gap: 16, // the tab button gap between icon and text
+          color: Colors.grey[800], // selected icon and text color
+          iconSize: 24,
+          selectedIndex: 0,
+          tabs: [
+            GButton(
+              backgroundColor: Colors.red.withOpacity(0.2),
+              textColor: Colors.red.withOpacity(0.8),
+              icon: FontAwesomeIcons.house,
+              iconActiveColor: Colors.red.withOpacity(0.8),
+              text: "Posts",
+            ),
+            GButton(
+              text: "Messages",
+              icon: FontAwesomeIcons.solidMessage,
+              backgroundColor: Colors.yellow.shade600.withOpacity(0.2),
+              textColor: Colors.yellow.shade800.withOpacity(0.8),
+              iconActiveColor: Colors.yellow.shade800.withOpacity(0.8),
+            ),
+            GButton(
+              text: "Profile",
+              icon: FontAwesomeIcons.solidUser,
+              backgroundColor: Colors.purple.withOpacity(0.2),
+              textColor: Colors.purple.withOpacity(0.8),
+              iconActiveColor: Colors.purple.withOpacity(0.8),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
